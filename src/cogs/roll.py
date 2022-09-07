22f4e17b63da8b2098b8b26e4ee55247c1c30c6e
Khilseith <78451857+Khilseith@users.noreply.github.com>
2022-09-07 15:37:18 -0700

    using builtin app_commands.Range instead of manual validation

--- src/cogs/roll.py
+++ src/cogs/roll.py
@@ -27,7 +27,7 @@ class Roll(commands.Cog):
         )
         await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
-    @app_commands.command(name="roll", description="Quickly Roll Some Dice")
+    @app_commands.command(name="quickroll", description="Quickly Roll Some Dice")
     @app_commands.describe(
         ammount="# of dice",
         sides="# of sides on the dice",
@@ -37,30 +37,11 @@ class Roll(commands.Cog):
     async def qroll(
         self,
         interaction: discord.Interaction,
-        ammount: int,
-        sides: int,
-        modifier: int = 0,
+        ammount: app_commands.Range[int, 1, 10],
+        sides: app_commands.Range[int, 1, 100],
+        modifier: app_commands.Range[int, -500, 500] = 0,
         goal: int = 0,
     ):
-        if (
-            ammount >= 10
-            or sides >= 100
-            or sides <= 1
-            or modifier >= 500
-            or modifier <= -500
-            or goal <= -500
-            or goal >= 500
-        ):
-            em = discord.Embed(
-                title="Error!",
-                description="""
-                Number of dice must be a number 1 - 10.
-                The Die/Dice can have 1-100 sides.
-                The Modifier and the Goal have to be between -500 and 500. """,
-                color=0xFF1100,
-            )
-            await interaction.response.send_message(embed=em, ephemeral=True)
-            return
 
         # Create the roll
         r = roll(f"{ammount}d{sides} + {modifier}")
