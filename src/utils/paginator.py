924d8b2052cf6d7681899cf7c99882270f5fbd68
Khilseith <78451857+Khilseith@users.noreply.github.com>
2022-09-16 14:32:16 -0700

    Added first/last page buttons to paginator, refactored button disabling

--- src/utils/paginator.py
+++ src/utils/paginator.py
@@ -8,6 +8,18 @@ class Paginator(discord.ui.View):
         self.page = 0
 
     @discord.ui.button(
+        label="<<-", custom_id="full_left", style=discord.ButtonStyle.primary
+    )
+    async def button_full_left(
+        self, interaction: discord.Interaction, _: discord.ui.Button
+    ):
+        self.page = 0
+        self.update_button_status()
+        return await interaction.response.edit_message(
+            embed=self.pages[self.page], view=self
+        )
+
+    @discord.ui.button(
         label="<-", custom_id="left", style=discord.ButtonStyle.primary, disabled=True
     )
     async def button_left(
@@ -22,16 +34,7 @@ class Paginator(discord.ui.View):
             self.page -= 1
 
         if self.page != pervious_page:
-            for child in self.children:
-                if not isinstance(child, discord.ui.Button):
-                    continue
-
-                match child.custom_id:
-                    case "left":
-                        child.disabled = self.page == 0
-                    case "right":
-                        child.disabled = self.page == len(self.pages) - 1
-
+            self.update_button_status()
             return await interaction.response.edit_message(
                 embed=self.pages[self.page], view=self
             )
@@ -48,18 +51,36 @@ class Paginator(discord.ui.View):
             self.page += 1
 
         if self.page != pervious_page:
-            for child in self.children:
-                if not isinstance(child, discord.ui.Button):
-                    continue
-
-                match child.custom_id:
-                    case "left":
-                        child.disabled = self.page == 0
-                    case "right":
-                        child.disabled = self.page == len(self.pages) - 1
-
+            self.update_button_status()
             return await interaction.response.edit_message(
                 embed=self.pages[self.page], view=self
             )
 
         return await interaction.response.defer()
+
+    @discord.ui.button(
+        label="->>", custom_id="full_right", style=discord.ButtonStyle.primary
+    )
+    async def button_full_right(
+        self, interaction: discord.Interaction, _: discord.ui.Button
+    ):
+        self.page = len(self.pages) - 1
+        self.update_button_status()
+        return await interaction.response.edit_message(
+            embed=self.pages[self.page], view=self
+        )
+
+    def update_button_status(self):
+        for child in self.children:
+            if not isinstance(child, discord.ui.Button):
+                continue
+
+            match child.custom_id:
+                case "full_left":
+                    child.disabled = self.page == 0
+                case "left":
+                    child.disabled = self.page == 0
+                case "right":
+                    child.disabled = self.page == len(self.pages) - 1
+                case "full_right":
+                    child.disabled = self.page == len(self.pages) - 1
